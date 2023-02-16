6c944721fd188eda0433060dc9a6b558b1d1d994
mark@oxil.co.uk <mark@oxil.co.uk>
2023-02-16 08:51:17 +0000

    Tweaks

--- ts/formatters/date-formatters.ts
+++ ts/formatters/date-formatters.ts
@@ -5,6 +5,35 @@ dayjs.extend(advancedFormat)
 
 let DateFormatters = {
 
+    durations: [
+        {
+            "key": "milliseconds",
+            "label": "Millisecond",
+            "multiplier": 1
+        },
+        {
+            "key": "seconds",
+            "label": "Second",
+            "multiplier": 1000
+        },
+        {
+            "key": "minutes",
+            "label": "Minute",
+            "multiplier": 60000
+        },
+        {
+            "key": "hours",
+            "label": "Hour",
+            "multiplier": 3600000
+        },
+        {
+            "key": "days",
+            "label": "Day",
+            "multiplier": 86400000
+        }
+    ],
+
+
     date: function (value, format) {
         return dayjs(value).format(format);
     },
@@ -19,7 +48,37 @@ let DateFormatters = {
     dateAdd(value, period, quantity) {
         let date = dayjs(value);
         return date.add(quantity, period);
+    },
+
+    // Format a duration
+    formattedDuration(value, sourceUnit = "milliseconds", maxExpressions = 10) {
+
+        // Firstly calculate the unit divisor
+        let unitDivisor = null;
+        DateFormatters.durations.forEach((duration: any) => {
+            if (duration.key == sourceUnit)
+                unitDivisor = duration.multiplier;
+        });
+
+
+        // if we found a unit divisor, work through the expressions
+        let expressions = [];
+        if (unitDivisor) {
+            for (let i = DateFormatters.durations.length - 1; i >= 0; i--) {
+                let durationValue = Number(value) / DateFormatters.durations[i].multiplier * unitDivisor;
+                if (durationValue >= 1) {
+                    let duration = Math.floor(durationValue);
+                    expressions.push(duration + " " + DateFormatters.durations[i].label + (duration > 1 ? "s" : ""));
+                }
+                if (expressions.length >= maxExpressions)
+                    break;
+            }
+        }
+
+        return expressions.join(" ");
+
     }
+
 
 };
 
