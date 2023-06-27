80e03802362be6866cf9a395d7aaab5b845472fb
mark@oxil.co.uk <mark@oxil.co.uk>
2023-06-27 12:26:04 +0100

    New value binder

--- ts/binders/value.ts
+++ ts/binders/value.ts
@@ -14,7 +14,80 @@ export default function Value(valueBinder) {
         if (el.hasAttribute('one-way')) {
             return;
         } else {
-            valueBinder.coreBind.call(this, el);
+
+            // Handle file input case as special case
+            if (el.tagName === 'INPUT' && el.type === 'file') {
+
+                let contentHandler = el.dataset.contentHandler;
+
+                var self = this;
+
+                let values = [];
+
+                // Add getter for value
+                Object.defineProperty(el, "value", {
+                    get: () => {
+                        return values;
+                    },
+                    set: (value) => {
+                    }
+                });
+
+
+                // Ensure we have callback
+                if (!this.callback) {
+                    this.callback = function (event: any) {
+                        if (event.target.files) {
+                            // Loop through each file
+                            let fileList = event.target.files;
+
+                            values = [];
+
+                            // If inline base 64 or inline text, process separately
+                            if (contentHandler == "inlineBase64" || contentHandler == "inlineText") {
+                                let totalFiles = fileList.length;
+                                let completedFiles = 0;
+                                for (var i = 0; i < totalFiles; i++) {
+                                    let reader = new FileReader();
+                                    let file = fileList[i];
+
+                                    // Process uploads until we have processed them all
+                                    reader.onload = function (loadedEvent) {
+                                        values.push({
+                                            name: file.name,
+                                            size: file.size,
+                                            type: file.type,
+                                            lastModified: file.lastModified,
+                                            content: loadedEvent.target.result
+                                        });
+                                        completedFiles++;
+                                        if (completedFiles == totalFiles)
+                                            self.publish();
+                                    }
+
+                                    // Call the appropriate function for inline content
+                                    if (contentHandler == "inlineBase64") {
+                                        reader.readAsDataURL(file);
+                                    } else {
+                                        reader.readAsText(file);
+                                    }
+
+                                }
+
+                            } else {
+                                values = fileList;
+                                self.publish();
+                            }
+                        }
+                    };
+                }
+
+                // Bind to change event
+                el.addEventListener("change", this.callback);
+
+            } else {
+                valueBinder.coreBind.call(this, el);
+            }
         }
     }
 
